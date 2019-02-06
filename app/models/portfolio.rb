4d524e02bd2a8dbf01b48040bceddc3f10d3afe3
nekofrozen <blackblackmaster@gmail.com>
2019-02-06 16:30:48 +0700

    Implemented concern for managing image data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,6 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
-
+  include Placeholder
   def self.angular
     where(subtitle: 'Angular')
   end
@@ -10,8 +10,8 @@ class Portfolio < ApplicationRecord
   after_initialize :set_defaults
 
   def set_defaults
-    self.main_image ||="https://place-hold.it/300x150"
-    self.thumb_image ||="https://place-hold.it/300x150"
+    self.main_image ||= Placeholder.image_generator(height: '300', width: '150')
+    self.thumb_image ||= Placeholder.image_generator(height: '300', width: '150')
   end
 end
 
