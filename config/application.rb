72de1dbf6844c58f1e9a85f2c84bc4e0256032dd
Nekofrozen <blackblackmaster@gmail.com>
2019-03-08 16:01:07 +0700

    Implemented initial connection to Twitter API

--- config/application.rb
+++ config/application.rb
@@ -19,16 +19,8 @@ Bundler.require(*Rails.groups)
 
 module MyPortfolio
   class Application < Rails::Application
-    #config.action_controller.permit_all_parameters = true
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    # Don't generate system test files.
+    config.eager_load_paths << "#{Rails.root}/lib"
     config.generators.system_tests = nil
   end
 end
